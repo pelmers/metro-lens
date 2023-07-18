c4f870253c9f86902f2e26a9a5da4c6f798cd1aa
Peter Elmers <peter.elmers@yahoo.com>
2023-07-18 17:12:28 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# [WoP](https://wop.pelmers.com)
+
+## aka: World of Polygons
+
+### Draw any shape and see how awful our world is...
+
+### Example
+
+TODO
+
+### Description
+
+TODO - short version and link to blog post
+
+### Usage
+
+TODO
+
+Data source: [WorldPop](https://www.worldpop.org)
+
+### Future work
+
+TODO
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+const ROOT = path.resolve(__dirname, "src");
+const DESTINATION = path.resolve(__dirname, "dist");
+
+const clientConfig = {
+  context: ROOT,
+
+  mode: process.env.BUILD_MODE || "development",
+  entry: {
+    index: "./client/index/main.tsx",
+  },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: DESTINATION,
+  },
+
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"],
+    modules: [ROOT, "node_modules"],
+  },
+
+  module: {
+    rules: [
+      /****************
+       * PRE-LOADERS
+       *****************/
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        use: "source-map-loader",
+      },
+      {
+        enforce: "pre",
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "tslint-loader",
+      },
+
+      /****************
+       * LOADERS
+       *****************/
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        use: "ts-loader",
+      },
+    ],
+  },
+
+  devtool: "cheap-module-source-map",
+  devServer: {},
+};
+
+const serverConfig = {
+  context: ROOT,
+  node: {
+    __filename: true,
+  },
+
+  mode: process.env.BUILD_MODE || "development",
+  entry: {
+    server: "./server/main.ts",
+  },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: DESTINATION,
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"],
+    modules: [ROOT],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: "ts-loader",
+      },
+    ],
+  },
+
+  devtool: "cheap-module-source-map",
+  target: "node",
+  externals: [nodeExternals()],
+};
+
+module.exports = [clientConfig, serverConfig];
