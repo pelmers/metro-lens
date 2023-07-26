0d7f54b27cc4a87e777c44dfd52c5a6b716a27c1
Peter Elmers <peter.elmers@yahoo.com>
2023-07-26 19:26:37 +0000

    Remove dist from repo

--- dist/client/index/fetchPopulation.d.ts
+++
@@ -1,6 +0,0 @@
-import { FeatureCollection } from "@turf/turf";
-import { StatValue } from "./MapStatsComponent";
-export declare function sleep(ms: number): Promise<unknown>;
-export declare const fetchPopulation: (borders: FeatureCollection<import("@turf/turf").Geometry | import("@turf/turf").GeometryCollection, {
-    [name: string]: any;
-}>, areaKm2: number) => Promise<StatValue>;
--- dist/server/rpcServer.d.ts
+++
@@ -1,3 +0,0 @@
-import ws from "ws";
-import { RpcServer } from "roots-rpc";
-export declare function createRpcServer(socket: ws): RpcServer;
